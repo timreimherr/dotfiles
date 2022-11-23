0172c0bb7494b8d66a164df766873818d357803c
Tim Reimherr <timreimherr@github.com>
2022-11-23 16:37:39 -0500

    remove failing command

--- .zshrc
+++ .zshrc
@@ -15,7 +15,6 @@ fi
 # enterprise2
 export PATH=~/enterprise2:$PATH
 export PATH=$PATH:/usr/local/go/bin
-eval "$(rbenv init -)"
 export GITHUB_HOSTNAME=$(hostname)
 export DEV_MODE=1
 export OVERLAY_VM_FILES=no
--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ git config --global push.default current
 git config --global alias.lo  "log --pretty='format:%C(auto)%h %C(green)%an %C(auto)%s' -10"
 
 while true; do
-  read -rep $'\nReady to continue and apply the symlinks? (y) This will overwrite ~/.zshrc' y
+  read -rep $'\nReady to continue and apply the symlinks? (y) This will overwrite ~/.zshrc ' y
   case "${y}" in
       [Yy]*)
 
@@ -26,4 +26,3 @@ if [ "$SHELL" != "/usr/bin/zsh" ]; then
     sudo apt install -y zsh
     zsh
 fi;
-
