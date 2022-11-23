720596c038242e749e8eacf8f41425a2df657ea3
Tim Reimherr <timreimherr@github.com>
2022-11-23 16:22:09 -0500

    overwrite file

--- install.sh
+++ install.sh
@@ -8,8 +8,18 @@ git config --global pager.branch false
 git config --global push.default current
 git config --global alias.lo  "log --pretty='format:%C(auto)%h %C(green)%an %C(auto)%s' -10"
 
+while true; do
+  read -rep $'\nReady to continue and apply the symlinks? (y) This will overwrite ~/.zshrc' y
+  case "${y}" in
+      [Yy]*)
+
+          break;;
+      *) echo "Please answer y or CTRL+c the script to abort everything";;
+  esac
+done
+
 # links
-ln -s ~/.dotfiles/.zshrc ~/
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 # zsh 
 if [ "$SHELL" != "/usr/bin/zsh" ]; then
