8226d012e1b9c2795386ecb29ed1c5c53b2a22ac
Tim Reimherr <timreimherr@github.com>
2022-12-02 10:47:12 -0500

    simplify install

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+# Sourced from https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+DIR_NAME="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # git config
 git config --global user.name "Tim Reimherr"
 git config --global user.email "timreimherr@github.com"
 git config --global core.editor vim
-git config --global pager.branch false
-git config --global push.default current
-git config --global alias.lo  "log --pretty='format:%C(auto)%h %C(green)%an %C(auto)%s' -10"
+git config --global pull.rebase false
 
-while true; do
-  read -rep $'\nReady to continue and apply the symlinks? (y) This will overwrite ~/.zshrc ' y
-  case "${y}" in
-      [Yy]*)
+# zsh
+## Install oh-my-zsh
+rm -rf "$HOME/.oh-my-zsh"
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
+export ZSH="${HOME}/.oh-my-zsh"
+export ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
 
-          break;;
-      *) echo "Please answer y or CTRL+c the script to abort everything";;
-  esac
-done
+## Install zsh plugins
+rm -rf "ZSH_CUSTOM/plugins/zsh-autosuggestions" 
+rm -rf "ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
-# links
-ln -fs ~/.dotfiles/.zshrc ~/.zshrc
+# link zshrc
+ln -sf "$DIR_NAME/zshrc" "$HOME/.zshrc"
 
 # zsh 
 if [ "$SHELL" != "/usr/bin/zsh" ]; then
